5f00c9cd5020e34116fc699ba034668c88abab36
MikKusch <mike.k1600@arcor.de>
2021-12-13 00:11:30 +0100

    Funktioniert

--- V354/main.py
+++ V354/main.py
@@ -5,13 +5,14 @@ from scipy.optimize import curve_fit
 
 
 zeit, spannung = np.genfromtxt('Daten.txt', unpack = True)
+zeit *= 1e-3
 
-def f(zeit, a, b, c):
-    return a*np.exp(b*zeit)+c
+def f(zeit, a, b):
+    return a*np.exp(b*zeit)
+
 
 params1, covariance_matrix1 = curve_fit(f, zeit[::2], spannung[::2])
 uncertainties1 = np.sqrt(np.diag(covariance_matrix1))
-
 
 
 params2, covariance_matrix2 = curve_fit(f, zeit[1::2], spannung[1::2])
@@ -19,11 +20,15 @@ uncertainties2 = np.sqrt(np.diag(covariance_matrix2))
 
 zeit_plot = np.linspace(zeit[0], zeit[-1], 1000)
 
-plt.plot(zeit, spannung, '.', label = 'Messwerte')
-#plt.plot(zeit_plot, f(zeit_plot, *params1), label = 'Regressionkurve')
+plt.plot(zeit, spannung, ':.', label = 'Messwerte')
+plt.plot(zeit_plot, f(zeit_plot, *params1), label = 'Regressionkurve')
+plt.plot(zeit_plot, f(zeit_plot, *params2), label = 'Regressionkurve')
 
-plt.xlabel(r'$\varphi/rad$')
-plt.ylabel(r'$A/V$')
+print(*params1)
+print(*params2)
+
+plt.xlabel(r'$t/\mu s$')
+plt.ylabel(r'$U/V$')
 
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
