1749b33ab9055de8de71f63b27cb860350ef403f
MikKusch <A9elSZ0K>
2022-01-16 22:42:18 +0100

    ...

--- V206/t_2.py
+++ V206/t_2.py
@@ -5,24 +5,23 @@ from scipy.optimize import curve_fit
 t_2, p_a, leist, t_1, p_b = np.genfromtxt('V206.txt', unpack = True)
 zeit = np.linspace(0, 840, 28)
 
-def f (zeit, a, b, c):
-    return a*zeit**2+b*zeit+c
+
+def g (zeit, a, b, c):
+    return a/(1+zeit**c*b)
 
 
-params, covariance_matrix = curve_fit(f, zeit, t_1)
-
-print(*params)
+params_2, covariance_matrix_2 = curve_fit(g, zeit, t_2)
 
 temp_plot = np.linspace(zeit[0], zeit[-1], 1000)
 
-plt.plot(zeit, t_1, '.', label = 'Messwerte')
-plt.plot(temp_plot, f(temp_plot, *params), label = 'Regressionskurve')
+print(*params_2)
+
+plt.plot(zeit, t_2, '.', label = 'Messwerte')
+plt.plot(temp_plot, g(temp_plot, *params_2), label = 'Regressionskurve')
 
 plt.xlabel(r't in S')
-plt.ylabel(r'$T_1 in K$')
+plt.ylabel(r'$T_2 in K$')
 
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('Graph_1.pdf')
-
-
+plt.savefig('Graph_2.pdf')
