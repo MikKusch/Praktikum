6dcda067b5bd37af397c3b85ee8d7a4c10d957a7
MikKusch <mike.k1600@arcor.de>
2022-02-02 17:38:31 +0100

    ...

--- V354/kond_erre.py
+++ V354/kond_erre.py
@@ -2,20 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-freq, rad, a = np.genfromtxt('Daten3.txt', unpack=True)
+freq, a, b = np.genfromtxt('Daten3.txt', unpack=True)
 
-plt.semilogy(freq, rad, '.', label='Messwerte')
 
-plt.xlabel(r'$f/kHz$')
+phi=a/b*2*np.pi
+print(phi)
+
+plt.semilogx(freq, phi, '.', label='Messwerte')
+
+plt.xlabel(r'$f/Hz$')
 plt.ylabel(r'$\phi/rad$')
+plt.xticks([10e3, 2*10e3, 3*10e3, 4*10e3, 5*10e3],
+           [r"$10^{4}$", r"$2\cdot 10^{4}$", r"$3\cdot 10^{4}$", r"$4\cdot 10^{4}$", r"$5\cdot 10^{4}$"])
 
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('Graph_4.pdf')
-plt.show()
+plt.close()
 
 
-plt.plot(freq[8:15:], rad[8:15:], '.', label='Messwerte')
+plt.plot(freq[5:17:], phi[5:17:], '.', label='Messwerte')
+plt.axhline(y=np.pi/4, xmin=0, xmax=1, label = r'$\nu_1$', ls = ':', c = 'mediumseagreen')
+plt.axhline(y=np.pi*3/4, xmin=0, xmax=1, label = r'$\nu_2$', ls = ':', c = 'mediumseagreen')
+plt.axhline(y=np.pi/2, xmin=0, xmax=1, label = r'$\nu_{res}$', ls = ':', c = 'red')
 plt.xlabel(r'$f/kHz$')
 plt.ylabel(r'$\phi/rad$')
 
