8bdbdacae86ec6486c787f000251df8073c0e2b9
MikKusch <mike.k1600@arcor.de>
2022-04-22 15:48:19 +0200

    Upload

---
+++ V702/main.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+
+zeit, spannung = np.genfromtxt('Daten.txt', unpack = True)
+zeit *= 1e-3
+
+
+def f(zeit, a, b):
+    return a*np.exp(-2*np.pi* b*zeit)
+
+
+params1, covariance_matrix1 = curve_fit(f, zeit[::2], spannung[::2])
+uncertainties1 = np.sqrt(np.diag(covariance_matrix1))
+
+
+params2, covariance_matrix2 = curve_fit(f, zeit[1::2], spannung[1::2])
+uncertainties2 = np.sqrt(np.diag(covariance_matrix2))
+
+zeit_plot = np.linspace(zeit[0], zeit[-1], 1000)
+
+plt.plot(zeit, spannung, ':.', label = 'Messwerte')
+plt.plot(zeit_plot, f(zeit_plot, *params1), label = 'Regressionkurve')
+plt.plot(zeit_plot, f(zeit_plot, *params2), label = 'Regressionkurve')
+
+plt.xlabel(r'$t/ms$')
+plt.ylabel(r'$U/V$')
+
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.show()
