c0e0f9467e60a01627161dfbe17ecf49ab62afa4
MikKusch <mike.k1600@arcor.de>
2022-04-25 00:04:50 +0200

    ...

--- V702/main.py
+++ V702/main.py
@@ -4,29 +4,52 @@ from scipy.optimize import curve_fit
 
 
 
-zeit, spannung = np.genfromtxt('Daten.txt', unpack = True)
-zeit *= 1e-3
+imp_rho = np.genfromtxt('Daten_Rhod.txt', unpack = True)
+imp_van = np.genfromtxt('Daten_Vana.txt', unpack = True)
+
+imp_rho = imp_rho-4.75
+imp_van = imp_van-9.5
+
+t_rho = np.linspace(0, 750, 50)
+t_van = np.linspace(0, 900, 30)
 
 
-def f(zeit, a, b):
-    return a*np.exp(-2*np.pi* b*zeit)
+def N(t, b, λ):
+    return λ*t+b
 
 
-params1, covariance_matrix1 = curve_fit(f, zeit[::2], spannung[::2])
-uncertainties1 = np.sqrt(np.diag(covariance_matrix1))
+
+params_rho, covariance_matrix_rho = curve_fit(N, t_rho, np.log(imp_rho))
+uncertainties_rho = np.sqrt(np.diag(covariance_matrix_rho))
+
+params_van, covariance_matrix_van = curve_fit(N, t_van, np.log(imp_van))
+uncertainties_van = np.sqrt(np.diag(covariance_matrix_van))
 
 
-params2, covariance_matrix2 = curve_fit(f, zeit[1::2], spannung[1::2])
-uncertainties2 = np.sqrt(np.diag(covariance_matrix2))
 
-zeit_plot = np.linspace(zeit[0], zeit[-1], 1000)
 
-plt.plot(zeit, spannung, ':.', label = 'Messwerte')
-plt.plot(zeit_plot, f(zeit_plot, *params1), label = 'Regressionkurve')
-plt.plot(zeit_plot, f(zeit_plot, *params2), label = 'Regressionkurve')
 
-plt.xlabel(r'$t/ms$')
-plt.ylabel(r'$U/V$')
+t_plot_rho = np.linspace(0, 750, 1000)
+t_plot_van = np.linspace(0, 900, 1000)
+
+
+plt.semilogy(t_rho, np.log(imp_rho), ':.', label = 'Messwerte')
+plt.semilogy(t_plot_rho, N(t_plot_rho, *params_rho) , label = 'Regressionkurve')
+
+
+plt.xlabel(r'$t/s$')
+plt.ylabel(r'$N/s^{-1}$')
+
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.show()
+
+plt.semilogy(t_van, np.log(imp_van), ':.', label = 'Messwerte')
+plt.semilogy(t_plot_van, N(t_plot_van, *params_van), label = 'Regressionkurve')
+
+
+plt.xlabel(r'$t/s$')
+plt.ylabel(r'$N/s^{-1}$')
 
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
