6d155330217b46bf53eec15aa4fc4de904a69a24
MikKusch <mike.k1600@arcor.de>
2022-06-14 01:00:54 +0200

    ...

--- V606/Bestimmung der Suszeptibilitäten.py
+++ V606/Bestimmung der Suszeptibilitäten.py
@@ -11,9 +11,6 @@ def lande(J, L, S):
     return (3*J*(J+1)+(S*(S+1)-L*(L+1)))/(2*J*(J+1))
 
 
-def magMo(rho, M):
-    return 2*(rho/M)*const.N_A
-
 #               J,   L,  S
 Dy = np.array([ 7.5, 5, 2.5])
 
@@ -37,4 +34,70 @@ dy_theo = (const.mu_0*const.physical_constants['Bohr magneton'][0]**2*lande(Dy[0
 print(gd_theo)
 print(nd_theo)
 print(dy_theo)
+
+F = 0.866
+
+Q_Nd = 0.0662
+Q_Gd = 0.115
+Q_Dy = 0.12
+
+#Mittelwert von der diff Wiederstand
+
+Nd_r = np.array([0.08, 0.095, 0.12])
+
+Gd_r = np.array([0.765, 0.765, 0.785])
+
+Dy_r = np.array([1.53, 1.56, 1.59])
+
+
+#Mittelwert
+
+Nd_r3 = np.array([3.4, 3.4, 3.405])
+
+Gd_r3 = np.array([3.365, 3.575, 3.57])
+
+Dy_r3 = np.array([3.38, 3.38, 3.39])
+
+
+Nd_U = np.array([0, 0, 0.07])
+Gd_U = np.array([0.1, 0.12, 0.15])
+Dy_U = np.array([0.05, 0.2, 0.34])
+
+
+def diff_r(x):
+    return np.mean(x)
+
+
+def sus_r(diff, R, F, Q):
+    return 2*(diff/R)*(F/Q)
+
+
+def sus_u(F, Q, U_br):
+    return 4*(F/Q)*(U_br/1000)
+
+Nd_U = diff_r(Nd_U)
+Gd_U = diff_r(Gd_U)
+Dy_U = diff_r(Dy_U)
+
+diff_Nd = diff_r(Nd_r)
+diff_Gd = diff_r(Gd_r)
+diff_Dy = diff_r(Dy_r)
+
+R3 = 1000
+
+print(diff_Nd, diff_Gd, diff_Dy)
+
+
+print('Susität von Nd: ', sus_r(diff_Nd, R3, F, Q_Nd))
+print('Susität von Gd: ', sus_r(diff_Gd, R3, F, Q_Gd))
+print('Susität von Dy: ', sus_r(diff_Dy, R3, F, Q_Dy))
+
+
+print('Susität von Nd: ', sus_u(F, Q_Nd, Nd_U))
+print('Susität von Gd: ', sus_u(F, Q_Gd, Gd_U))
+print('Susität von Dy: ', sus_u(F, Q_Dy, Dy_U))
+
+
+
+
 
